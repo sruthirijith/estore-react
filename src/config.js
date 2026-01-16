8a0798d30bd9f96de8dbe01626eda46e6eaf27b2
Sruthi VP <sruthisr21@gmail.com>
2026-01-16 10:14:24 +0530

    config.js updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
 // API Configuration
-export const BASE_URL = "http://13.61.19.222/api";
+// export const BASE_URL = "http://13.61.19.222/api";
+
+// window.BASE_URL = "http://13.61.19.222/api";
+
+window.BASE_URL = process.env.REACT_APP_API_URL;
