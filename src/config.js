d8a7dbdf5b6778697e290499dbe3f6d4674faecc
Sruthi VP <sruthisr21@gmail.com>
2026-01-16 11:09:40 +0530

    cloud change

--- src/config.js
+++ src/config.js
@@ -3,4 +3,6 @@
 
 // window.BASE_URL = "http://13.61.19.222/api";
 
-window.BASE_URL = process.env.REACT_APP_API_URL;
+// window.BASE_URL = process.env.REACT_APP_API_URL;
+
+window.BASE_URL = window.__ENV__.BASE_URL;
